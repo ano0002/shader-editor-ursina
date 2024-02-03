40d292374004d3f103f1698d4a29a502ad6afcd6
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2024-02-03 22:29:40 +0100

    Removed ignored files

--- cupcake/utils/canvas.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-
-class Canvas(tk.Canvas):
-    def __init__(self, master, *args, **kwargs):
-        super().__init__(master, *args, **kwargs)
-        self.master = master
-        self.base = master.base
--- cupcake/utils/menubutton.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-
-class Menubutton(tk.Menubutton):
-    def __init__(self, master, *args, **kwargs):
-        super().__init__(master, *args, **kwargs)
-        self.master = master
-        self.base = master.base
